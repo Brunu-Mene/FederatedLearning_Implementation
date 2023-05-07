99f942ddd88b9fbdc8d317ad5d4675fdeb68c6fd
Bruno Menegaz Acerbi <bruno.menegaz261@gmail.com>
2023-05-07 18:47:49 -0300

    Removing usl functions

--- aux.py
+++ aux.py
@@ -26,27 +26,3 @@ def load_mnist_byCid(cid):
 
     return data, labels
 
-def load_mnist_bypath(paths, verbose = -1):
-    data = []
-    labels = []
-
-    for (i, imgpath) in enumerate(paths):
-        # load the image and extract the class labels
-        im_gray = cv2.imread(imgpath, cv2.IMREAD_GRAYSCALE)
-        image = np.array(im_gray).flatten()
-        label = imgpath.split(os.path.sep)[-2]
-
-        # aqui é feita a escala da img para [0, 1] para diminuir o impacto do brilho de cada pixel
-        data.append(image/255)
-        labels.append(label)
-        # show an update every `verbose` images
-        if verbose > 0 and i > 0 and (i + 1) % verbose == 0:
-            print("[INFO] processed {}/{}".format(i + 1, len(paths)))
-    # return a tuple of the data and labels
-    return data, labels
-
-
-def loadClientData(cid):      
-    img_path = 'mnist_data/'
-    image_paths = list(paths.list_images(img_path))
-
