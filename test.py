125e1091d6f785d2aaeedd8e4bc5e641fb853e49
bmacerbi_local <bruno.menegaz261@gmail.com>
2023-05-18 08:00:50 -0300

    tesTest

---
+++ test.py
@@ -0,0 +1,11 @@
+import os
+
+root = os.getcwd()
+
+os.system(f"git add {root}/README.md")
+os.system('git commit -am "tesReadMe"')
+
+os.system(f"git add {root}/test.py")
+os.system('git commit -am "tesTest"')
+
+os.system("git push origin master")
